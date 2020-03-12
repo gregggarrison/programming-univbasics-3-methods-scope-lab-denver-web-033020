26f5881d8fda39fd6d5b17d0b2e043b9946a1518
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-12 20:14:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,21 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!\n"
   puts phrase
 end
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
   puts status
+end
+
+def link_to_remote
+  status = "It's Dangerous To Go Alone! Take This.\n"
+  puts status
+end
+
+def all phrases
+  puts mario
+  puts toadstool
+  puts link 
 end
